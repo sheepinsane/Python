93ddccfdcee1ad27e19472d01c42a6d7d5bfc944
sheepinsane <sheepinsane@gmail.com>
2023-07-25 17:02:09 +0800

    change forder for docker

---
+++ FastAPI/FastAPI/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+    
+@app.get("/API")
+def read_root():
+    return {"Hello": "API"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
